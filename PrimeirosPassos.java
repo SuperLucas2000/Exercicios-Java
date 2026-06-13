1f4a3456804aef34983e5d55d0b1d24070748f7b
Lucas <l.matteus.baptista@gmail.com>
2026-06-13 16:21:09 -0300

    Adicionar comentario

--- PrimeirosPassos.java
+++ PrimeirosPassos.java
@@ -1,6 +1,12 @@
+
+// Exibe Ola mundo, e o nome.
 public class PrimeirosPassos {
     public static void main(String[] args) {
+
+        // Printar Ola Mundo
         System.out.println("Ola Mundo");
+
+        // Printar Meu nome e Lucas
         System.out.println("Meu nome e Lucas");
     }
 }
