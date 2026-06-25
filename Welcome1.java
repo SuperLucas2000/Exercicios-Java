089511a03490cf0e36a5c8a036d8f9a98a790ab6
Lucas <l.matteus.baptista@gmail.com>
2026-06-25 09:36:47 -0300

    adicionado print de texto

--- Welcome1.java
+++ Welcome1.java
@@ -1,3 +1,9 @@
-public class Welcome1 {
+//Programa de impressão de texto.
 
-}
+public class Welcome1 {
+    // método main inicia a execução do aplicativo java
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Java Programming!");
+    } // fim do método main
+} // fim da classe Welcome1
