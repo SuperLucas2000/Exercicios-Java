84f731ce777ec4915723c2b2dcaec58e6cccfb0e
Lucas <l.matteus.baptista@gmail.com>
2026-06-25 12:57:24 -0300

    Exibir várias linhas com uma instrução

--- Welcome3.java
+++ Welcome3.java
@@ -1,3 +1,9 @@
+// Imprimindo múltiplas linhas de texto com uma única instrução.
+
 public class Welcome3 {
-    
-}
+    // Método main inicia a execução do aplicativo Java.
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome\nto\nJava\nProgramming!");
+    }// fim do método main
+}// fim da classe Welcome3
